0cb87cbd43afbf5143ad610f37b71b8b4ee958f6
Ubuntu <ubuntu@ip-172-31-84-227.ec2.internal>
2020-11-22 18:38:28 +0000

    added in comments

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+
+# Created by: Ryan Walsh
+# Created on: Novemeber 2020
+# This is the "Hello, World!" program, but with nice style
+
+
+def main():
+    # this functiion prints to console
+    print("Hello, World!")
+    
+    
+if __name__ == "__main__":
+    main()
+    
